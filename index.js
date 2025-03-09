9b6cbd9399203cc58ebab66acd11ec22267a5c92
uwhyauuah <uwhyauuah@gmail.com>
2025-03-09 11:37:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const { createVLESSServer } = require("@3kmfi6hp/nodejs-proxy");
 const {v4} = require("uuid");
-// 定义端口和 UUID
+const express = require('express')
+const app = express()
 const port = 443;
-const uuid = '87a3e815-7e0f-4bca-aca1-c06a57a862a8';
+const uuid = v4;
 
 // 调用函数启动 VLESS 服务器
 createVLESSServer(port, uuid);
 
-console.log(`vless://${uuid}@127.0.0.1:${port}?encryption=none&security=none&fp=randomized&type=ws&path=%2F#TunnelAce - Free`)
+app.get('/', (req, res) => {
+    res.send(`vless://${uuid}@127.0.0.1:${port}?encryption=none&security=none&fp=randomized&type=ws&path=%2F#TunnelAce - Free`);
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
