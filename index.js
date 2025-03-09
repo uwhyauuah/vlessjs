d26788f9978908b77c72222eb9f53b7e67be306f
uwhyauuah <uwhyauuah@gmail.com>
2025-03-09 11:31:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { createVLESSServer } = require("@3kmfi6hp/nodejs-proxy");
 const {v4} = require("uuid");
 // 定义端口和 UUID
 const port = 443;
-const uuid = v4;
+const uuid = '87a3e815-7e0f-4bca-aca1-c06a57a862a8';
 
 // 调用函数启动 VLESS 服务器
 createVLESSServer(port, uuid);
